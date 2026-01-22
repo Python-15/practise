42d815f792942e9a0ca221b2e0c2dba3848c29e0
Python-15 <raaz.aditya15@gmail.com>
2026-01-22 17:45:39 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  console.log(c)
+const a=2;
+function af ()
+{
+    var c = 12
+}
+
+  return (
+    <></>
+  )
+}
+
+export default App
---
+++ vite-project/src/test.js
@@ -0,0 +1,5 @@
+console.log(a)
+let a=2;
+{
+    var c = 12
+}
